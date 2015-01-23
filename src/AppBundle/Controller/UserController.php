552fd68a4bbebca21ff3eb3fe71fceee0b203210
nadim-jahangir-m4 <nadim.jahangir@metafour.com>
2015-01-23 12:08:30 +0600

    Project created

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\User;
+
+class UserController extends Controller
+{
+	const DATE_FORMAT = 'Y-m-d';
+	
+	private function userExists($email) 
+	{
+		$em = $this->getDoctrine()->getManager();
+		$user = $em->getRepository('AppBundle:User')->find($email);
+		return $user ? true : false;
+	}
+	
+	private function userToJson($user) 
+	{
+		$data = array(
+			'email' => $user->getEmail(),
+			'name' => $user->getName(),
+			'image' => $user->getImage(),
+			'memberSince' => $user->getMemberSince()->format(self::DATE_FORMAT)
+		);
+		return json_encode($data);
+	}
+	
+    /**
+     * @Route("/user/create", name="userCreate")
+     * @Method({"POST"})
+     */
+    public function createUserAction()
+    {
+    	$data = json_decode($this->getRequest()->getContent(), true);
+    	
+    	$email = $data['email'];
+    	
+    	if ($this->userExists($email)) 
+    	{
+    		$response = new Response();
+    		$response->setContent("Another user with email $email exists");
+    		$response->setStatusCode(409);
+    		return $response;
+    	}
+    	
+    	$user = new User();
+    	$user->setEmail($data['email']);
+    	$user->setPassword($data['password']);
+    	
+    	if (isset($data['name'])) $user->setName($data['name']);
+    	else $user->setName('');
+    	
+    	if (isset($data['image'])) $user->setImage($data['image']);
+    	else $user->setImage('');
+    	
+    	$user->setMemberSince(new \DateTime("now"));
+    	$user->setIsActive(true);
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$em->persist($user);
+    	$em->flush();
+    	
+        return new Response($this->userToJson($user));
+    }
+    
+    /**
+     * @Route("/user/view/{email}", name="userView")
+     * @Method({"POST"})
+     */
+    public function viewUserAction($email)
+    {
+    	$user = $this->getDoctrine()->getRepository('AppBundle:User')->find($email);
+    	if (!$user)
+    	{
+    		throw $this->createNotFoundException("User with email $email does not exist");
+    	}
+    	
+    	return new Response($this->userToJson($user));
+    }
+    
+    /**
+     * @Route("/user/edit/{email}", name="userEdit")
+     * @Method({"POST"})
+     */
+    public function editUserAction($email)
+    {
+    	$data = json_decode($this->getRequest()->getContent(), true);
+    	
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $em->getRepository('AppBundle:User')->find($email);
+    	if (!$user)
+    	{
+    		throw $this->createNotFoundException("User with email $email does not exist");
+    	}
+    	
+    	if (isset($data['email'])) {
+    		$e = $data['email'];
+    		if (!empty($e) && $this->userExists($e)) 
+    		{
+    			$response = new Response();
+    			$response->setContent("Another user with email $e exists");
+    			$response->setStatusCode(409);
+    			return $response;
+    		}
+    	}
+    	
+    	if (isset($e) && !empty($e))
+    	{
+	    	$user->setEmail($e);
+    	}
+    	
+    	if (isset($data['name']))
+    	{
+    		$name = $data['name'];
+    		if (!empty($name)) $user->setName($name);
+    	}
+    	
+    	if (isset($data['image']))
+    	{
+	    	$image = $data['image'];
+	    	if (!empty($image)) $user->setImage($image);
+    	}
+    	
+    	if (isset($data['password']))
+    	{
+	    	$password = $data['password'];
+	    	if (!empty($password)) $user->setPassword($data['password']);
+    	}
+    	
+    	$em->flush();
+    	
+    	$user = $em->getRepository('AppBundle:User')->find(isset($newEmail) ? $newEmail : $email);
+    	
+    	return new Response($this->userToJson($user));
+    }
+    
+    /**
+     * @Route("/user/login/{email}", name="userLogin")
+     * @Method({"POST"})
+     */
+    public function loginUserAction($email)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $em->getRepository('AppBundle:User')->find($email);
+    	if (!$user)
+    	{
+    		throw $this->createNotFoundException("User with email $email does not exist");
+    	}
+    	
+    	return new Response($this->userToJson($user));
+    }
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,253 @@
+<?php 
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * AppBundle\Entity\User
+ *
+ * @ORM\Table(name="Users")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\UserRepository")
+ */
+class User implements UserInterface, \Serializable
+{
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     * @ORM\Id
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+    
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $image;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $memberSince;
+    
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    public function __construct()
+    {
+        $this->isActive = true;
+        // may not be needed, see section on salt below
+        // $this->salt = md5(uniqid(null, true));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSalt()
+    {
+        // you *may* need a real salt depending on your encoder
+        // see section on salt below
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->email,
+            $this->password,
+        	$this->name,
+        	$this->memberSince,
+        	$this->image,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->email,
+            $this->password,
+        	$this->name,
+        	$this->memberSince,
+        	$this->image,
+            // see section on salt below
+            // $this->salt
+        ) = unserialize($serialized);
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \test $image
+     * @return User
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \test 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set memberSince
+     *
+     * @param \DateTime $memberSince
+     * @return User
+     */
+    public function setMemberSince($memberSince)
+    {
+        $this->memberSince = $memberSince;
+
+        return $this;
+    }
+
+    /**
+     * Get memberSince
+     *
+     * @return \DateTime 
+     */
+    public function getMemberSince()
+    {
+        return $this->memberSince;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
