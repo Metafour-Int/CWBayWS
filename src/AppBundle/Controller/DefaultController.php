89686cf0873773d20f39ed713184e5755477f3a4
Nadim Jahangir <nadim.jahangir@metafour.com>
2015-01-26 15:50:52 +0600

    category view api added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Category;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,20 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return new Response();
+        return new Response('OK');
+    }
+    
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction()
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$parent = $this->getDoctrine()->getRepository('AppBundle:Category')->find(13);
+    	
+    	
+    	
+    	
+    	return new Response($parent->getChildren()->count());
     }
 }
